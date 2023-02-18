8c1a5bf841183cce87d0903a2842bfb927c1771e
aoporoy <aopo56@gmail.com>
2023-02-19 02:01:29 +0600

    First Commit

---
+++ src/Components/index.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import {Button, Card} from 'react-bootstrap';
+
+export default function Index() {
+    let cardList = [];
+
+    let [list, listHandle] = useState([]);
+    let [load, loadHandle] = useState(true);
+    let [stateChange, stateChangeHandle] = useState(false);
+    
+    useEffect(()=>{
+        setTimeout(() => {
+            fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => {
+                if(!res.ok){
+                    throw Error("Not Response!");
+                }else{ 
+                    return res.json();
+                }
+            })
+            .then((data) => {
+                loadHandle(false);
+                listHandle(data);
+            })
+            .catch((err) => {
+
+            })
+        }, 2000);
+    },[stateChange]);
+    cardList = list.map((singleData) =>
+                <Card className='' key={singleData.id}>
+                    <Card.Body>
+                        <Card.Title>{singleData.title}</Card.Title>
+                        <Card.Text>{singleData.body}</Card.Text>
+                        <div>
+                            <Button>See More</Button>
+                            <Button variant='danger ms-3 '>Report</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            )
+    
+  return (
+    <div className='card-wrapper'>
+        {load && <h5 className='text-center'>Loading Data</h5>}
+        {cardList}
+    </div>
+  )
+}
